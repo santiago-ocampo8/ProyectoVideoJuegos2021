400693a403288fb7661ffd2c9d3d5f92a5252a86
santiago-ocampo8 <51138167+santiago-ocampo8@users.noreply.github.com>
2021-10-06 00:25:59 -0500

    Creation zombie an script

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour
+{
+
+    NavMeshAgent Agente;
+    // Start is called before the first frame update
+    void Start()
+    {
+     this.Agente=gameObject.GetComponent<NavMeshAgent>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        seguir(GameObject.Find("FPSController").transform.position);
+    }
+
+    void seguir(Vector3 objetivo){
+        this.Agente.destination=objetivo;
+    }
+}
